8fcedd6f9f9751a73921d8d24db14b671bace00e
Fran8016 <francisco.garcia.garcia.al@iespoligonosur.org>
2025-10-06 18:04:05 +0000

    EJ06

---
+++ EJ06.java
@@ -0,0 +1,10 @@
+public class EJ06 {
+    public static void main (String[] args) {
+        String nombreUsuario = "Fran";
+        int edadUsuario = 19;
+        String mesDeNacimiento = "Febrero";
+        System.out.println(nombreUsuario);
+        System.out.println(edadUsuario);
+        System.out.println(mesDeNacimiento);
+    }
+}
