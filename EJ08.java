d727e8a72be66ebf0906e7b08ce665dfbb55dd57
Fran8016 <francisco.garcia.garcia.al@iespoligonosur.org>
2025-10-07 17:22:03 +0000

    EJO8

---
+++ EJ08.java
@@ -0,0 +1,12 @@
+public class EJ08{
+    public static void main(String[] args) {
+        int edad = 19;
+        double precio = 7.30;
+        char letra = 'F';
+        boolean esValido = true;
+        System.out.println(edad);
+        System.out.println(precio);
+        System.out.println(letra);
+        System.out.println(esValido);
+    }
+}
