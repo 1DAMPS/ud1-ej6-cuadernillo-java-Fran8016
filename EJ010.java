da698cf78b05d9f60897bc4f610d71b1f4643db1
Fran8016 <francisco.garcia.garcia.al@iespoligonosur.org>
2025-10-08 10:31:12 +0000

    Ej010

---
+++ EJ010.java
@@ -0,0 +1,8 @@
+public class EJ010{
+    public static void main(String[] args) {
+         byte edad = 12;
+         short año = 900;
+        System.out.println(edad);
+        System.out.println(año);
+    }
+}
