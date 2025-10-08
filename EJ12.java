efca920cf1f924cc4f903b021eca380eb73a5a22
Fran8016 <francisco.garcia.garcia.al@iespoligonosur.org>
2025-10-08 10:41:08 +0000

    EJ12

---
+++ EJ12.java
@@ -0,0 +1,7 @@
+public class EJ12{
+    public static void main(String[] args){
+        float pi = 3.14;
+        //El error es incompatible types: possible lossy conversion form double float
+        System.out.println(pi);
+    }
+}
