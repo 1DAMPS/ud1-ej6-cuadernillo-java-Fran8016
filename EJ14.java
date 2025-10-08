26869d351674e8937454b211661ff9b33422164b
Fran8016 <francisco.garcia.garcia.al@iespoligonosur.org>
2025-10-08 11:08:14 +0000

    EJ14

---
+++ EJ14.java
@@ -0,0 +1,6 @@
+public class EJ14{
+    public static void main(String[] args){
+        final byte dias = 7;
+        System.out.println("La semana tiene "  + dias + " dias en la semana");
+    }
+}
