cb70da47b91978edcb70042f8e2dd1e6c6a60862
Fran8016 <francisco.garcia.garcia.al@iespoligonosur.org>
2025-10-08 11:15:25 +0000

    ej15

---
+++ EJ15.java
@@ -0,0 +1,8 @@
+public class EJ15{
+public static void main(String[] args){
+    int max = 2147483647;
+    int max1 = max + 1;
+    System.out.println(max1);
+    //Al pasar el limite maximo vuelve al principio por eso sale negativo
+}
+}
