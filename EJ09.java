1beefea96b1a4bb71bdd0bb1c0fc4be7437dda56
Fran8016 <francisco.garcia.garcia.al@iespoligonosur.org>
2025-10-08 10:23:08 +0000

    Ej09

---
+++ EJ09.java
@@ -0,0 +1,8 @@
+public class EJ09{
+    public static void main(String[] args) {
+        int edad;
+        System.out.println(edad);
+        //No funciona ya que el la variable edad no esta inicializada, es decir existe la variable pero no tiene ningun valor
+    }
+
+}
