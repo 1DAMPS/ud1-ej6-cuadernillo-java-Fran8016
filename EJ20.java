627ae8ed88aa1143170310aa0f3c3d4a3532c239
Fran8016 <francisco.garcia.garcia.al@iespoligonosur.org>
2025-10-15 12:12:45 +0000

    ej20

---
+++ EJ20.java
@@ -0,0 +1,12 @@
+/**Este es un pequeño programa que calcula el area de una circunferencia a traves del radio
+ * @author Fran
+ * @version 1.0
+ */
+public class EJ20{
+    public static void main(String[] args){
+        int radio = 5;
+        double area = 3.1416 * radio * radio;
+        System.out.println(area);
+    }
+}
+    
