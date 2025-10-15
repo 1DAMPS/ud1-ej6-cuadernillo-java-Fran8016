41b042eaf379fcfb872a50c3e85609e884d4ed83
Fran8016 <francisco.garcia.garcia.al@iespoligonosur.org>
2025-10-15 12:04:35 +0000

    ej19

---
+++ EJ19.java
@@ -0,0 +1,10 @@
+public class EJ19{
+    public static void main(String[] args){
+        int radio = 5;
+        //Declara la variable "radio" que tiene de valor 5
+        double area = 3.1416 * radio * radio;
+        //Declara el valor de area a traves de un calculo
+        System.out.println(area);
+        //Muestra en pantalla el valor de area
+    }
+}
